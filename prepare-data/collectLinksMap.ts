2d7a5b51716ed68d3fa679714581d3094c966406
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-03-06 18:38:24 +0300

    Collect links

--- prepare-data/collectLinksMap.ts
+++ prepare-data/collectLinksMap.ts
@@ -1,23 +1,32 @@
 import { writeFile } from 'fs';
+import { join } from 'path';
 import { getArray, getDocumentElement, getLink, getText } from '../src/utils';
 
 const mainUrl = 'https://simplywall.st';
 const RuMarketUrl = 'https://simplywall.st/stocks/ru';
 
-const collectLinks = async () => {
-    const doc = await getDocumentElement(RuMarketUrl);
+const collectLinks = async (url: string) => {
+    console.log('Parsing the first page...');
+
+    const doc = await getDocumentElement(url);
 
     const lastPageLink = getLink(doc, '[data-cy-id="last"]');
     const lastPage = Number((lastPageLink?.match(/page=(.+)/) ?? [])[1]) ?? 1;
 
-    const links = getLinks(doc);
+    let links = getLinks(doc);
 
-    console.log(links);
+    for (let i = 2; i <= lastPage; i++) {
+        console.log(`Getting links from page ${i}`);
+        const pageLinks = await collectLinksFromPage(`${url}?page=${i}`);
+        links = links.concat(pageLinks);
+    }
 
-    writeFile('links.json', JSON.stringify(links), (err) => {
-        if (err) throw err;
-        console.log('complete');
-    });
+    return links;
+};
+
+const collectLinksFromPage = async (url: string) => {
+    const doc = await getDocumentElement(url);
+    return getLinks(doc);
 };
 
 const getLinks = (doc: Document) => {
@@ -25,7 +34,8 @@ const getLinks = (doc: Document) => {
         doc,
         '[data-cy-id="stocks-table-row"] td:nth-child(2) > a'
     ).map((el) => {
-        const link = el.getAttribute('href') ?? undefined;
+        let link = el.getAttribute('href') ?? undefined;
+        link = link ? `${mainUrl}${link}` : undefined;
         const ticker = getText(el, 'b');
         const name = getText(el, 'span');
         return {
@@ -36,4 +46,10 @@ const getLinks = (doc: Document) => {
     });
 };
 
-collectLinks();
+(async () => {
+    const links = await collectLinks(RuMarketUrl);
+    writeFile(join(__dirname, 'links.json'), JSON.stringify(links), (err) => {
+        if (err) throw err;
+        console.log('complete');
+    });
+})();
